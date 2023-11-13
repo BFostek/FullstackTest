19b8bba56d8eaae220e81887be588646916e4fe4
BFostek <bfmbreno@hotmail.com>
2023-11-13 04:09:24 -0300

    Create conection with backend

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,18 +1,110 @@
 "use client";
-import { Autocomplete, Box, Button, Card, CardMedia, Grid, Icon, Paper, TextField, Typography, alpha } from "@mui/material";
-import _ from "lodash";
+import React, { useCallback, useEffect, useState } from 'react';
+import { Container, Paper, Grid, Box, Typography, Card, Icon, TextField } from '@mui/material';
+import Autocomplete from '@mui/material/Autocomplete';
 import CloudIcon from '@mui/icons-material/Cloud';
-import { useCallback, useEffect, useState } from "react";
-import { Container } from "@mui/material";
+import { alpha } from '@mui/system';
+import _, { parseInt } from 'lodash';
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
+
+const iconMap = {
+  '01d': "http://openweathermap.org/img/wn/01d.png",
+  '02d': "http://openweathermap.org/img/wn/02d.png",
+  '03d': "http://openweathermap.org/img/wn/03d.png",
+  '04d': "http://openweathermap.org/img/wn/04d.png",
+  '09d': "htpp://openweathermap.org/img/wn/09d.png",
+  '10d': "http://openweathermap.org/img/wn/10d.png",
+  '11d': "http://openweathermap.org/img/wn/11d.png",
+  '13d': "http://openweathermap.org/img/wn/13d.png",
+  '50d': "http://openweathermap.org/img/wn/50d.png",
+  '01n': "http://openweathermap.org/img/wn/01n.png",
+  '02n': "http://openweathermap.org/img/wn/02n.png",
+  '03n': "http://openweathermap.org/img/wn/03n.png",
+  '04n': "http://openweathermap.org/img/wn/04n.png",
+  '09n': "htpp://openweathermap.org/img/wn/09n.png",
+  '10n': "http://openweathermap.org/img/wn/10n.png",
+  '11n': "http://openweathermap.org/img/wn/11n.png",
+  '13n': "http://openweathermap.org/img/wn/13n.png",
+  '50n': "http://openweathermap.org/img/wn/50n.png",
+}
+const WeatherCard = ({ time, temperature, index, len, icon }) => {
+  let cardStyle = { padding: 2, borderRadius: "0", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "space-between" }
+  if (index == 0) {
+    cardStyle = { ...cardStyle, borderRadius: "15px 0 0 15px" }
+  }
+  if (index == len - 1) {
+    cardStyle = { ...cardStyle, borderRadius: "0 15px 15px 0" }
+  }
+  return (
+    <Card sx={cardStyle}>
+      <Typography variant="h6">
+        {time}
+      </Typography>
+      <Box component="img" src={iconMap[icon]} sx={{ width: "50px" }} />
+      <Typography variant="h5">
+        {temperature}°C
+      </Typography>
+    </Card>
+  );
+}
+
+const NextDaysCard = ({ day, weather, icon }) => (
+  <Grid item>
+    <Card sx={{ padding: 4, background: "#fbfcfe", borderRadius: 0, display: "flex", flexDirection: "column", alignItems: "center" }}>
+      <Grid container justifyContent="center" alignItems="center">
+        <Grid item xs={3}>
+          <Typography variant="h6">
+            {day}
+          </Typography>
+        </Grid>
+        <Grid item xs={2}>
+          <Box component="img" src={iconMap[icon]} sx={{ width: "50px" }} />
+        </Grid>
+        <Grid item xs={4}>
+          <Typography variant="h6">
+            {weather}
+          </Typography>
+        </Grid>
+        <Grid item xs={3}>
+          <Typography variant="h6">
+
+          </Typography>
+        </Grid>
+      </Grid>
+    </Card>
+  </Grid>
+);
 
 export default function Home() {
   const [options, setOptions] = useState<any>([]);
+  const [option, setOption] = useState<any>({});
   const [value, setValue] = useState<any>("");
-
+  const [currentWeather, setCurrentWeather] = useState<any>();
+  const [nextHours, setNextHours] = useState<any>([]);
+  const [nextDays, setNextDays] = useState<any>([]);
+  useEffect(() => {
+    if (!option) return;
+    if (!option.latitude) return;
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/weather?lat=` + option.latitude + "&lon=" + option.longitude, { mode: 'cors' }).then((response) => response?.body?.getReader().read())
+      .catch((err) => (err))
+      .then((result: any) => {
+        if (!result) return;
+        if (!result.value) return;
+        let weather = JSON.parse(new TextDecoder("utf-8").decode(result?.value));
+        if (weather.length == 0) return;
+        let currentWeatherObj = weather[0];
+        let nextHoursArr = weather.slice(1, 6);
+        let nextDaysArr = weather.slice(-5);
+        setCurrentWeather(currentWeatherObj);
+        setNextHours(nextHoursArr);
+        setNextDays(nextDaysArr);
+      }).
+      catch((err) => err);
+  }, [option]);
   const getOptionsDelayed = useCallback(
     _.debounce((text, callback) => {
       setOptions([]);
-      if(!text) return;
+      if (!text) return;
       if (text.length < 3) return;
       getCities(text).then(callback);
     }, 200),
@@ -42,31 +134,29 @@ export default function Home() {
   };
   return (
     <Container>
-      <Paper sx={{
-        minWidth: "500px", marginTop: 3, borderRadius: "15px", boxShadow: 10
-      }}>
+      <Paper sx={{ minWidth: "500px", marginTop: 3, boxShadow: 10 }}>
         <Grid container>
-          <Grid item xs={8} sx={{ backgroundColor: "#00658f", borderRadius: "15px 0 0 15px" }}>
+          <Grid item md={8} sm={12} sx={{ backgroundColor: "#00658f" }}>
             <Grid container justifyContent="center" direction="row" >
               <Grid item xs={8} sx={{ paddingX: 3, marginTop: 2 }}>
                 <Autocomplete
                   freeSolo
                   filterOptions={(x) => x}
-                  onChange={(e: any) => setValue(e.target.innerText)}
-
-                  onInputChange={(e, value) => {
-                    setValue(value);
-                  }}
-                  loading={options.length === 0}
+                  onChange={(e: any) => setOption(
+                    options.find((obj: any) => obj.fullName == e.target.innerText))}
+                  onInputChange={(_, value) => setValue(value)}
+                  loading={options.length === 0 && value.length > 2}
                   options={options ? options.map((obj: any) => obj.fullName) : []}
                   sx={{
-
-                    bgcolor: alpha("#fbfcfe", 0.7), borderRadius: "15px", '& .Mui-focused': {
+                    bgcolor: alpha("#fbfcfe", 0.7),
+                    borderRadius: "15px",
+                    '& .Mui-focused': {
                       borderColor: '#fbfcfe',
                       color: '#fbfcfe',
                       borderWidth: 2,
                       borderRadius: "15px"
-                    }, '& .MuiOutlinedInput-root': {
+                    },
+                    '& .MuiOutlinedInput-root': {
                       '& fieldset': {
                         borderColor: '#fbfcfe',
                         color: '#fbfcfe',
@@ -101,22 +191,25 @@ export default function Home() {
               </Grid>
             </Grid>
             <Grid container justifyContent="center" sx={{ minHeight: "30vh", marginY: 5 }}>
-              <Grid item >
+              <Grid item>
                 <Box sx={{ display: "flex", color: "#fbfcfe", flexDirection: "column", alignItems: "center" }}>
                   <Typography variant="h3">
-                    Montreal
+                    {currentWeather ?
+                      currentWeather.city.name + ", " + currentWeather.city.country
+                      :
+                      "..."}
                   </Typography>
                   <Typography variant="h1">
-                    14°C
+                    {currentWeather ? parseInt(currentWeather.temperature) : "??"}°C
                   </Typography>
                   <Typography variant="h5">
-                    Cloudy
+                    {currentWeather ? currentWeather.weatherCondition.description : "..."}
                   </Typography>
                 </Box>
               </Grid>
             </Grid>
             <Grid container sx={{
-              bgcolor: alpha("#fbfcfe", 0.3), justifyContent: "center", borderRadius: "0 15px 0 15px"
+              bgcolor: alpha("#fbfcfe", 0.3), justifyContent: "center",
             }}>
               <Grid item sx={{ marginY: "15px" }}>
                 <Typography variant="h6" color="#fbfcfe">
@@ -124,226 +217,43 @@ export default function Home() {
                 </Typography>
               </Grid>
               <Grid container sx={{ marginBottom: 2, justifyContent: "center" }}>
-                <Grid item xs={2}>
-                  <Card sx={{ padding: 2, borderRadius: "20px 0 0 20px", display: "flex", flexDirection: "column", alignItems: "center" }}>
-                    <Typography variant="h6">
-                      12:00
-                    </Typography>
-                    <Icon>
-                      <CloudIcon />
-                    </Icon>
-                    <Typography variant="h5">
-                      12°C
-                    </Typography>
+                {nextHours.map((item, index, arr) => (
+                  <Grid item key={index} xs={2}>
+                    <WeatherCard
+                      icon={item.weatherCondition.icon}
+                      time={item.timestamp.split("T")[1].slice(0, 5)}
+                      temperature={parseInt(item.temperature)}
+                      index={index}
+                      len={arr.length} />
 
-                  </Card>
-                </Grid>
-                <Grid item xs={2}>
-                  <Card sx={{ padding: 2, borderRadius: 0, display: "flex", flexDirection: "column", alignItems: "center" }}>
-                    <Typography variant="h6">
-                      12:00
-                    </Typography>
-                    <Icon>
-                      <CloudIcon />
-                    </Icon>
-                    <Typography variant="h5">
-                      12°C
-                    </Typography>
-
-                  </Card>
-                </Grid>
-                <Grid item xs={2}>
-                  <Card sx={{ padding: 2, borderRadius: 0, display: "flex", flexDirection: "column", alignItems: "center" }}>
-                    <Typography variant="h6">
-                      12:00
-                    </Typography>
-                    <Icon>
-                      <CloudIcon />
-                    </Icon>
-                    <Typography variant="h5">
-                      12°C
-                    </Typography>
-
-                  </Card>
-                </Grid>
-                <Grid item xs={2}>
-                  <Card sx={{ padding: 2, borderRadius: 0, display: "flex", flexDirection: "column", alignItems: "center" }}>
-                    <Typography variant="h6">
-                      12:00
-                    </Typography>
-                    <Icon>
-                      <CloudIcon />
-                    </Icon>
-                    <Typography variant="h5">
-                      12°C
-                    </Typography>
-
-                  </Card>
-                </Grid>
-
-                <Grid item xs={2}>
-                  <Card sx={{ padding: 2, borderRadius: "0 20px 20px 0", display: "flex", flexDirection: "column", alignItems: "center" }}>
-                    <Typography variant="h6">
-                      12:00
-                    </Typography>
-                    <Icon>
-                      <CloudIcon />
-                    </Icon>
-                    <Typography variant="h5">
-                      12°C
-                    </Typography>
-
-                  </Card>
-                </Grid>
+                  </Grid>
+                ))}
               </Grid>
             </Grid>
           </Grid>
-          <Grid item xs={4} >
-            <Box sx={{ minHeight: "100%", minWidth: "200px", bgcolor: "#00658f", borderRadius: "0 15px 15px 0" }}>
+          <Grid item md={4} sm={12} xs={12} >
+            <Box sx={{ minHeight: "100%", minWidth: "200px", bgcolor: "#00658f" }}>
               <Grid container justifyContent="center" direction="column" sx={{ padding: 2 }}>
                 <Grid item sx={{ marginY: "15px" }}>
                   <Typography variant="h5" color="#fbfcfe">
                     Weather of Next Days
                   </Typography>
                 </Grid>
-                <Grid item >
-                  <Card sx={{ padding: 4, background: "#fbfcfe", borderRadius: "20px 20px 0 0", display: "flex", flexDirection: "column", alignItems: "center" }}>
-                    <Grid container justifyContent="center" alignItems="center">
-                      <Grid item xs={3}>
-                        <Typography variant="h6">
-                          Seg
-                        </Typography>
-                      </Grid>
-                      <Grid item xs={2}>
-                        <Icon>
-                          <CloudIcon />
-                        </Icon>
-                      </Grid>
-                      <Grid item xs={4}>
-                        <Typography variant="h6">
-                          Cloudy
-                        </Typography>
-                      </Grid>
-                      <Grid item xs={3}>
-                        <Typography variant="h6">
-                          12:00
-                        </Typography>
-                      </Grid>
-                    </Grid>
-                  </Card>
-                </Grid>
-                <Grid item >
-                  <Card sx={{ padding: 4, background: "#fbfcfe", borderRadius: "0", display: "flex", flexDirection: "column", alignItems: "center" }}>
-                    <Grid container justifyContent="center" alignItems="center">
-                      <Grid item xs={3}>
-                        <Typography variant="h6">
-                          Ter
-                        </Typography>
-                      </Grid>
-                      <Grid item xs={2}>
-                        <Icon>
-                          <CloudIcon />
-                        </Icon>
-                      </Grid>
-                      <Grid item xs={4}>
-                        <Typography variant="h6">
-                          Cloudy
-                        </Typography>
-                      </Grid>
-                      <Grid item xs={3}>
-                        <Typography variant="h6">
-                          12:00
-                        </Typography>
-                      </Grid>
-                    </Grid>
-                  </Card>
-                </Grid>
+                {nextDays.map((data, index) => (
+                  <NextDaysCard index={index} day={(new Date(data.timestamp)).toLocaleString('default', {
+                    weekday: 'short'
+                  })} weather={parseInt(data.temperature) + "°C"}
+                    icon={data.weatherCondition.icon}
+                  />
+                ))}
 
-                <Grid item >
-                  <Card sx={{ padding: 4, background: "#fbfcfe", borderRadius: "0", display: "flex", flexDirection: "column", alignItems: "center" }}>
-                    <Grid container justifyContent="center" alignItems="center">
-                      <Grid item xs={3}>
-                        <Typography variant="h6">
-                          Qua
-                        </Typography>
-                      </Grid>
-                      <Grid item xs={2}>
-                        <Icon>
-                          <CloudIcon />
-                        </Icon>
-                      </Grid>
-                      <Grid item xs={4}>
-                        <Typography variant="h6">
-                          Cloudy
-                        </Typography>
-                      </Grid>
-                      <Grid item xs={3}>
-                        <Typography variant="h6">
-                          12:00
-                        </Typography>
-                      </Grid>
-                    </Grid>
-                  </Card>
-                </Grid>
-
-                <Grid item >
-                  <Card sx={{ padding: 4, background: "#fbfcfe", borderRadius: "0", display: "flex", flexDirection: "column", alignItems: "center" }}>
-                    <Grid container justifyContent="center" alignItems="center">
-                      <Grid item xs={3}>
-                        <Typography variant="h6">
-                          Qui
-                        </Typography>
-                      </Grid>
-                      <Grid item xs={2}>
-                        <Icon>
-                          <CloudIcon />
-                        </Icon>
-                      </Grid>
-                      <Grid item xs={4}>
-                        <Typography variant="h6">
-                          Cloudy
-                        </Typography>
-                      </Grid>
-                      <Grid item xs={3}>
-                        <Typography variant="h6">
-                          12:00
-                        </Typography>
-                      </Grid>
-                    </Grid>
-                  </Card>
-                </Grid>
-
-                <Grid item >
-                  <Card sx={{ padding: 4, background: "#fbfcfe", borderRadius: "0 0 20px 20px", display: "flex", flexDirection: "column", alignItems: "center" }}>
-                    <Grid container justifyContent="center" alignItems="center">
-                      <Grid item xs={3}>
-                        <Typography variant="h6">
-                          Sex
-                        </Typography>
-                      </Grid>
-                      <Grid item xs={2}>
-                        <Icon>
-                          <CloudIcon />
-                        </Icon>
-                      </Grid>
-                      <Grid item xs={4}>
-                        <Typography variant="h6">
-                          Cloudy
-                        </Typography>
-                      </Grid>
-                      <Grid item xs={3}>
-                        <Typography variant="h6">
-                          12:00
-                        </Typography>
-                      </Grid>
-                    </Grid>
-                  </Card>
-                </Grid>
               </Grid>
             </Box>
           </Grid>
         </Grid>
-      </Paper >
+      </Paper>
     </Container>
-  )
-}
+  );
+};
+
+
